5f67e9ba9f117535d724bbbd50b4b35e13b6dade
WaddyAgnew <waddyagnew@gmail.com>
2022-05-09 22:03:35 -0400

    good habbits

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ var formslide = document.getElementById("div_form");
 function endingmessage(){
     formslide.style.right = "-800px";
 }
+
+/* look into more sliding pages check to see if z-index will help........
+-- just in case you for get, rember in main.js that the Id can do just as much as the style will allow in the regular style id nothing more
+
+*/
