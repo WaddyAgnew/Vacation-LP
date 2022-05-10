1116d4a54d8312dd9154cb29d95937f3173686d6
WaddyAgnew <waddyagnew@gmail.com>
2022-05-10 11:37:19 -0400

    Slide complete

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
-
+/*
 var formslide = document.getElementById("div_form");
 
 function endingmessage(){
-    formslide.style.right = "-800px";
+    formslide.style.right = "-100px";
+    formslide.style.zIndex = "-1";
+}
+*/
+var formslide = document.getElementById("div");
+
+function endingmessage(){
+    formslide.classList.toggle("fade_form");
 }
 
-/* look into more sliding pages check to see if z-index will help........
--- just in case you for get, rember in main.js that the Id can do just as much as the style will allow in the regular style id nothing more
 
+/* look into more sliding pages check to see if z-index will help........
+-- just in case you for get, rember in main.js that the Id can do just as much as the style will allow in the regular style id nothing more --
+
+--probably mostly a css fix--
 */
