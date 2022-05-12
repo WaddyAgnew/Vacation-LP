e1d4670ecda62132308ac570632629328b53bf1b
WaddyAgnew <waddyagnew@gmail.com>
2022-05-12 12:15:33 -0400

    ending slide

--- main.js
+++ main.js
@@ -7,9 +7,12 @@ function endingmessage(){
 }
 */
 var formslide = document.getElementById("div");
-
+var greetslide = document.getElementById("MainGreet");
+var gudbyeslide = document.getElementById("MainGudbye");
 function endingmessage(){
     formslide.classList.toggle("fade_form");
+    greetslide.classList.toggle("fade_greet");
+    gudbyeslide.classList.toggle("fade_gudbye")
 }
 
 
